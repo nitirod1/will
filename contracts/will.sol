cb571a731c3bfe248f3380e7f228e878809576a4
=subder <=>
2023-03-24 00:55:41 +0700

    update factory mint will

--- contracts/will.sol
+++ contracts/will.sol
@@ -1,57 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
-
-contract Will is ERC721, Ownable {
-    string internal name;
+contract Will {
+    uint256 internal tokenId;
+    address internal owner;
     address internal beneficiary ;
-    string internal uri;
-    bool internal active;
-
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIdCounter;
 
     mapping(address => uint256[]) digitalAssetId;
     mapping(address => uint256[]) realAssetId;
 
     constructor(
-        string memory _name,
+        uint256 _tokenId,
+        address _owner , 
         address _beneficiary 
-    ) ERC721("King MongKut's University", "KMUTT") {
-        name = _name;
+    ) {
+        owner = _owner;
+        tokenId = _tokenId;
         beneficiary = _beneficiary;
-        active = false;
-    }
-
-    modifier isActive(){
-        require(active == true , "Not active");
-        _;
-    }
-
-    function safeMint(address to,uint256 tokenId) public onlyOwner {
-        _safeMint(to, tokenId);
-    }
-
-    function activeWill() public onlyOwner{
-        active = true;
-    }
-
-     function withdrawERC20(
-        address _tokenAddress,
-        uint256 _amount
-    ) external isActive {
-        IERC20Metadata token = IERC20Metadata(_tokenAddress);
-
-        uint256 balance = token.balanceOf(address(this));
-
-        require(balance >= _amount, "Not Enought Balance !");
-
-        token.transfer(beneficiary, _amount);
-
     }
 
 }
--- contracts/willFactory.sol
+++ contracts/willFactory.sol
@@ -1,28 +1,71 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 import "./will.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract willFactory{
-    mapping(address => address[])public wills;
+
+contract willFactory is ERC721, AccessControl {
+    // owner of will
+    mapping(address => address) public owner;
+    // status of will
+    mapping(address => bool) public wills;
+    // tokenId of will
+    mapping(uint256 => address) public tokenIds;
+
+    using Counters for Counters.Counter;
+
+    bytes32 public constant OWNER_ROLE = keccak256("OWNER_ROLE");
+    Counters.Counter private _tokenIdCounter;
     
+    constructor() ERC721("MyToken", "MTK") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(OWNER_ROLE, msg.sender);
+    }
 
-    function getWill(address _owner) public view returns(address [] memory){
-        return wills[_owner];
+    function getStatusWill(address _adrWill ) public view returns(bool){
+        return wills[_adrWill];
+    }
+
+    function getOwner(address _adrWill) public view returns(address){
+        return owner[_adrWill];
     }
 
     event NewWill(address locker, address owner);
 
     function newWill(
-        string memory _name,
+    uint256 _tokenId,
     address _owner , 
     address _beneficiary 
-    )public{
+    )public onlyRole(OWNER_ROLE){
         // create new will
-        Will will = new Will(_name,_beneficiary);
+        Will will = new Will(_tokenId,_owner,_beneficiary);
 
-        wills[_owner].push(address(will));
+        owner[address(will)] = _owner;
+
+        wills[address(will)] = false;
+
+        tokenIds[_tokenId] = address(will);
 
         // emit new will event
         emit NewWill(address(will), _owner);
     }
-} 
+
+   function safeMint(address to) public onlyRole(OWNER_ROLE) {
+        uint256 tokenId = _tokenIdCounter.current();
+        _safeMint(to, tokenId);
+        _tokenIdCounter.increment();
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, AccessControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
